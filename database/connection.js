84a9182f4df306996d436af889a4f587ddaf8515
Campocruz <cosmacampo93@yahoo.it>
2026-06-30 20:16:51 +0200

    create env file, update .gitignore file, add connection module, create firts route with a status message, add the second route to show all users and last add the route to get the specific user by id

---
+++ database/connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+const credetial = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
+console.log(credetial);
+
+const connection = mysql.createConnection(credetial)
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database', err);
+    return;
+  }
+  console.log('Connection to database');
+})
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -17,6 +17,45 @@ app.use(express.static('public'));
 // Import JSON format for read body request
 app.use(express.json());
 
+// Define connection
+const connection = require('./database/connection')
+
 /*
       END IMPORT SECTION
 */
+
+// Get Home
+app.get('/', (req, res) => {
+  res.json({
+    messaggio: "Server del mio Blog"
+  })
+})
+
+
+app.get('/users', (req, res) => {
+  const sql = `SELECT * FROM users`
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: 'Error DB connection' })
+    res.json(results)
+  })
+})
+
+
+app.get('/users/:id', (req, res) => {
+
+  const id = req.params.id
+
+  const sql = `SELECT * FROM users WHERE id = ?`
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Error DB connection' })
+    if (results.length === 0) return res.status(404).json({ message: 'not found' })
+    res.json(results[0])
+  });
+});
+
+// Listen Server
+app.listen(port, () => {
+  console.log(`Server listening http://localhost:${port}`);
+})
